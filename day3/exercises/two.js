b93c5d0475aba59421b2e837c4d724eda6417db6
earyzhe <earyzhe@gmail.com>
2019-04-03 12:30:03 +0200

    Exercise two is done

---
+++ day3/exercises/two.js
@@ -0,0 +1,28 @@
+delay(goodbye, 3000);
+delay(hello, 3000);
+
+function goodbye() {
+    console.log("Hello!");
+}
+
+function hello() {
+    console.log("Goodbye!");
+}
+
+function delay(delayedFunction, time) {
+    var today = new Date();
+    var start = today.getSeconds();
+    var end;
+
+    console.log("Start delay time " + start);
+
+    setTimeout(completion, time);
+
+    function completion() {
+        delayedFunction();
+        today = new Date();
+        end = today.getSeconds();
+        console.log("End time " + end);
+        console.log("Delay time was " + (end - start));
+    }
+}
