94f33a1e76bb2292221c1a2297a5cc4449fb290b
earyzhe <earyzhe@gmail.com>
2019-04-04 14:16:24 +0200

    exercise three finished

--- day4/exercises/three.js
+++ day4/exercises/three.js
@@ -7,9 +7,23 @@
 
 // Assumes that the given value is an arry of numnbers;
 
+var testArray = [1, 2, -1, -90, 10];
+
+console.log(getLessThanZero(testArray));
+
+
 function getLessThanZero(numberArray){
+
     var newArray = numberArray;
 
-    for (var index = 0; index < array.length; index++) {
+    for (var index = 0; index < newArray.length; index++) {
+
+        if ( newArray[index] > 0 ){
+            
+            newArray.splice(index, 1);
+            index--;
+         }
     }
+    return newArray;
 }
+
