b8d8c18a51abfdaa45b9de2c04289732988c513a
earyzhe <earyzhe@gmail.com>
2019-04-11 21:44:34 +0200

    last commit of the day

--- week2/kitty_carousel/script.js
+++ week2/kitty_carousel/script.js
@@ -1,97 +1,47 @@
 (function () {
 
     var transitionIntervalMs = 3000;
-    var transitionDurationMs = 1000;
-    var counter = 1;
-
-    // motion is from right to left with a duration of 5 seconds
+    
     var pussyCats = document.getElementsByClassName('pussy');
+    var dots = document.getElementsByClassName('dot');
+    var counter = 0;
 
-    var refArray = []
-
-    for (var i = 0; i < pussyCats.length; i++) {
-        refArray.push(i);
-    }
-    refArray.push(refArray.shift());
-    refArray.push(refArray.shift());
-    refArray.push(refArray.shift());
-
+    /// To start the motion from right to left with a interval of 5 seconds
     setTimeout(rotate, transitionIntervalMs);
 
-    function rotate(){
+    document.addEventListener('transitionend', removeCallback);
 
-        console.log(refArray);
+    function removeCallback(event){ 
+       if ( event.target.classList.contains("exit") ){
+            event.target.classList.remove("exit");
+            /// restart the function to resume the carosel;
+            setTimeout(rotate, transitionIntervalMs);
+       }
+    }
 
-        for (var i = 0; i < pussyCats.length; i++) {
-            
-            if ( i == 0  ){
-                console.log('in exit');
-                exitScreen(pussyCats[refArray[0]]);
-            }
-            else if ( i == 1 ){
-                console.log('in on');
-                onScreen(pussyCats[refArray[1]]);
-            }
-            else {
-                defaultPosition(pussyCats[refArray[2]]);
-                defaultPosition(pussyCats[refArray[3]]);;
-            }
+    function rotate() {
+        console.log('removing pussy ' + pussyCats[counter].id);        //move the photo from onscreen to exit.
+        pussyCats[counter].classList.add("exit");
+        pussyCats[counter].classList.remove("onScreen");
+        setDot();
+        counter++;
+        if (counter >= pussyCats.length) {
+            //check if its the last photo
+            //and restart the counter
+            counter = 0;
         }
-
-        refArray.push(refArray.shift())
-        // refArray.unshift(refArray.pop());
-        
-        counter ++;
-
-        setTimeout(rotate, transitionIntervalMs);
+        //move the next photo to onscreen
+        console.log('adding pussy ' + pussyCats[counter].id);        //move the photo from onscreen to exit.
+        pussyCats[counter].classList.add("onScreen");
     }
 
-    function defaultPosition(pussy){
-        // console.log('removeing ' + pussy.id);
-        pussy.classList.remove('exit');
-        pussy.classList.remove('onScreen');
+    function setDot(){
+        for (var i = 0; i < dots.length; i++) {
+            dots[i].classList.remove('on');
+        }
+        dots[counter].classList.add('on');
     }
-
-    function onScreen(pussy){
-        // console.log('onscreen ' + pussy.id)
-        pussy.classList.remove('exit');
-        pussy.classList.add('onScreen');
-    }
-
-    function exitScreen(pussy){
-        // console.log('exiting ' + pussy.id)
-        pussy.classList.remove('onScreen');
-        pussy.classList.add('exit');
-    }
-
-    function transitionEnd(){
-        
-    }
-
 })();
 
-  
 
-        function rotate(){
-            console.log(counter);        
-    
-            if ( counter < pussyCats.length - 1){ 
-    
-                exitScreen(pussyCats[counter]);
-                defaultPosition(pussyCats[counter + 1])
-    
-            }else if( counter == pussyCats.length){
-                defaultPosition(pussyCats[counter]);
-                counter = 0;
-            }
-            else if ( counter == 0 ){
-                onScreen(pussyCats[counter]);
-            }
-            else{
-                defaultPosition(pussyCats[counter + 1]);
-            }
-    
-            counter ++;
-    
-            setTimeout(rotate, transitionIntervalMs);
-        }
+
