ebfd1c5ef30c7c7ed15f5ecffe62d49a77a27f5f
earyzhe <earyzhe@gmail.com>
2019-04-12 07:44:00 +0200

    Dots looking slick

--- week2/kitty_carousel/script.js
+++ week2/kitty_carousel/script.js
@@ -5,26 +5,45 @@
     var pussyCats = document.getElementsByClassName('pussy');
     var dots = document.getElementsByClassName('dot');
     var counter = 0;
-
-    /// To start the motion from right to left with a interval of 5 seconds
-    setTimeout(rotate, transitionIntervalMs);
-
     document.addEventListener('transitionend', removeCallback);
 
+
+    /// To start the motion from right to left with a interval of 5 seconds
+    // setTimeout(rotate, transitionIntervalMs);
+
+    // Start the carosel
+    startCarosel();
+
+    function startCarosel(){
+
+        pussyCats[counter].classList.add("start");
+        setDot();
+        setTimeout(removeStart, transitionIntervalMs);
+
+        // remove the start parameters and start the main rotation
+        function removeStart(){
+            pussyCats[counter].classList.remove("start");
+            rotate();
+        }
+    }
+
+
     function removeCallback(event){ 
-       if ( event.target.classList.contains("exit") ){
+        if ( event.target.classList.contains("exit") ){
             event.target.classList.remove("exit");
             /// restart the function to resume the carosel;
             setTimeout(rotate, transitionIntervalMs);
-       }
+        }
     }
 
     function rotate() {
-        console.log('removing pussy ' + pussyCats[counter].id);        //move the photo from onscreen to exit.
+        console.log('removing pussy ' + pussyCats[counter].id);
+        
+        //move the photo from onscreen to exit.
         pussyCats[counter].classList.add("exit");
         pussyCats[counter].classList.remove("onScreen");
-        setDot();
         counter++;
+        
         if (counter >= pussyCats.length) {
             //check if its the last photo
             //and restart the counter
@@ -32,14 +51,19 @@
         }
         //move the next photo to onscreen
         console.log('adding pussy ' + pussyCats[counter].id);        //move the photo from onscreen to exit.
+        setDot();
         pussyCats[counter].classList.add("onScreen");
     }
 
     function setDot(){
         for (var i = 0; i < dots.length; i++) {
             dots[i].classList.remove('on');
+            var inner = dots[i].childNodes[0];
+            inner.classList.remove('on');
         }
         dots[counter].classList.add('on');
+        var innerAdd = dots[counter].childNodes[0];
+        innerAdd.classList.add('on');
     }
 })();
 
