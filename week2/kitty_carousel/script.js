3cfde794ba8b5d610d654247f9889f93ef84cc9f
earyzhe <earyzhe@gmail.com>
2019-04-12 14:22:14 +0200

    buttons are logging the correct dot

--- week2/kitty_carousel/script.js
+++ week2/kitty_carousel/script.js
@@ -5,14 +5,34 @@
     var pussyCats = document.getElementsByClassName('pussy');
     var dots = document.getElementsByClassName('dot');
     var counter = 0;
+
+    // Event delegation
     document.addEventListener('transitionend', removeCallback);
 
 
     /// To start the motion from right to left with a interval of 5 seconds
     // setTimeout(rotate, transitionIntervalMs);
 
+    setupDots();
+
     // Start the carosel
     startCarosel();
+
+    function setupDots(){
+        // for (var i = 0; i < dots.length; i++) {
+        //     dots[i].addEventListener('click', dotClick);
+        // }
+
+        for (var i = 0; i < dots.length; i++) {
+            // console.log(dots[i] + ' ' + event.target);
+            (function (i) {
+                dots[i].addEventListener('click', function(e){
+                    console.log(i);
+                    event.stopPropagation();
+                }); 
+            })(i);
+        }
+    }
 
     function startCarosel(){
 
@@ -27,7 +47,7 @@
         }
     }
 
-
+    // Event delegation
     function removeCallback(event){ 
         if ( event.target.classList.contains("exit") ){
             event.target.classList.remove("exit");
@@ -36,9 +56,7 @@
         }
     }
 
-    function rotate() {
-        console.log('removing pussy ' + pussyCats[counter].id);
-        
+    function rotate() {        
         //move the photo from onscreen to exit.
         pussyCats[counter].classList.add("exit");
         pussyCats[counter].classList.remove("onScreen");
@@ -50,7 +68,6 @@
             counter = 0;
         }
         //move the next photo to onscreen
-        console.log('adding pussy ' + pussyCats[counter].id);        //move the photo from onscreen to exit.
         setDot();
         pussyCats[counter].classList.add("onScreen");
     }
