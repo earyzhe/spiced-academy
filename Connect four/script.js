f0ce1c48ca45b6a91ceb6b35ed47fefdede11e07
earyzhe <earyzhe@gmail.com>
2019-04-17 17:10:19 +0200

    adding coins is done

--- Connect four/script.js
+++ Connect four/script.js
@@ -1,9 +1,35 @@
 (function () {
 
     var isRedTurn = false;
-    var clNames = {
 
-    } ;
+    $('.column').mousedown(function (event) {
+        var column = $(event.currentTarget);
 
+        for (var i = 0; i < column.children().length; i++) {
+            
+            var slot = column.children().eq(i);
 
+            if ( i == column.children().length - 1 && !slot.hasClass('yellow') && !slot.hasClass('red')){
+                if ( isRedTurn ){
+                    slot.addClass('red');
+                }
+                else{
+                    slot.addClass('yellow');
+                }
+                break;
+            }
+
+            if ( slot.hasClass('yellow') || slot.hasClass('red') ) {
+
+                if ( isRedTurn ){
+                    slot.prev().addClass('red');
+                }
+                else{
+                    slot.prev().addClass('yellow');
+                }
+                break;
+            }
+        } 
+        isRedTurn = !isRedTurn;
+    });
 })();
