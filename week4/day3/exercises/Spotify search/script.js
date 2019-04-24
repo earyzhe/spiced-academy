661f2bdfb4cbd6038bf425c67cf9569191133593
earyzhe <earyzhe@gmail.com>
2019-04-24 17:39:27 +0200

    Spotify finished

--- week4/day3/exercises/Spotify search/script.js
+++ week4/day3/exercises/Spotify search/script.js
@@ -13,30 +13,37 @@
             query: userInput,
             type: dropDownItem
         };
-        
-        makeApiCall(api ,data);
-       
+        makeApiCall(api ,data, userInput, dropDownItem);
     });
 
 
-    $('#more-button').on('click', function () {
+    $('#more-button').on('click', function () {        
         makeApiCall(next, null);
     });
 
 
-    function makeApiCall(url ,data){
-
+    function makeApiCall(url ,data, userInput, dropDownItem){
+        console.log('url is ' + url);
         $.ajax({
             type: "GET",
             url: url,
             data: data,
             success: function (responseData) {
-
-                // if next exists do the thing on the right
-                next = responseData.next && responseData.next.replace('https://api.spotify.com/v1/search', api);
-
+                
                 responseData = responseData.artists || responseData.albums;
 
+                if ( responseData.items.length > 0){
+                    $('#more-button').addClass('show');
+                    $('#results-state').text( dropDownItem + ' results for ' + userInput);
+                    
+                }else{
+                    $('#more-button').removeClass('show');
+                }
+
+                // if next exists do the thing on the right
+                console.log(responseData.next);
+                next = responseData.next && responseData.next.replace('https://api.spotify.com/v1/search', url);
+                
                 for (let index = 0; index < responseData.items.length; index++) {
                     const element = responseData.items[index];
                     addRowToScreen(element);
@@ -50,7 +57,6 @@
         if (item && item.images[0]){
             var external = '<a href=' + item.external_urls.spotify + '>';
             var image = item.images[0].url || 'http://cdn.osxdaily.com/wp-content/uploads/2019/03/spotify-icon.jpg';
-            console.log(image);
             var prefix = '<div class="tile"><img src=';
             var suffix = '</h3></div></a>';
             var hTML = external + prefix + image + '><h3>' + item.name + suffix;
