d3e99aa71425e055c7cd19a589c2f689ebf74bf5
earyzhe <earyzhe@gmail.com>
2019-05-12 04:54:56 +0200

    Tests done

--- testing/exercises/2/albums.test.js
+++ testing/exercises/2/albums.test.js
@@ -8,29 +8,6 @@ test('album names are in alphabetical order', () => {
     spotify.search.mockResolvedValue(testValues);
 
     return getAlbumNames('meat loaf').then(albums => { 
-        expect(albums).toBe(albums);
+        expect(albums).toBe(albums.sort());
     });
 });
-
-
-
-// const { getAlbumNames } = require("./albums");
-// const spotify = require("./spotify");
-
-// jest.mock("./spotify");
-
-// test("album names are in alphabetical order", () => {
-//     spotify.search.mockResolvedValue({
-//         albums: {
-//             items: [
-//                 { name: "album1" },
-//                 { name: "calbum3" },
-//                 { name: "dalbum4" },
-//                 { name: "galbum1" }
-//             ]
-//         }
-//     });
-//     return getAlbumNames("meat loaf").then(albumNames => {
-//         expect(albumNames).toEqual(albumNames.sort());
-//     });
-// });
--- testing/exercises/3/fn.js
+++ testing/exercises/3/fn.js
@@ -1,2 +1,16 @@
-module.exports = function fn() {
+module.exports = function fn(value) {
+
+    if (typeof value == "string"){
+        return value.split('').reverse().join('');
+    }
+    else if (Array.isArray(value)){
+
+        for (let index = 0; index < value.length; index++) {
+            value[index] = fn(value[index]);
+        }
+        return value;
+    }
+    else{
+        return null;
+    }
 };
